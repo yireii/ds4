cbd5504cdfa78fa796db6c37dc656c4e285478d6
yireii <yireiiabrego8@gmail.com>
2024-09-03 19:01:28 -0500

    lab3

---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            client.FirstName = "Su_Nombre";
+            client.LastName = "Su_Apellido";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+
+        }
+    }
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+        
+        public string GetFullName()
+        {  return FirstName+ "" + LastName; }
+    }
+}
