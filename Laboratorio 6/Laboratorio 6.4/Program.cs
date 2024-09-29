95036a61fcb7bbb8acacce6630778c716ae59fff
yireii <yireiiabrego8@gmail.com>
2024-09-29 13:54:27 -0500

    ﻿Laboratorio 6 Completado

---
+++ Laboratorio 6/Laboratorio 6.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Acceso negado - no cumpmle con el  criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acceso Concedido");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        checkAge(18);
+    }
+}
